0981f7c66e3341890eccd7dce124691e28c00eb5
Everett S <eas342@email.arizona.edu>
2016-06-30 17:14:55 -0700

    I adjusted run_ncdhaas to properly use the isimcv3 configuration file. Also I changed the execute command to making a dictionary of MMM, PMM etc. for the various flats. I had to use shell=True in order to make the command to ncdhas work properly with different input files.

--- run_ncdhas_list.py
+++ run_ncdhas_list.py
@@ -1,6 +1,6 @@
 '''
-1:  -ipc -cl -cf    DONE
-2:  +ipc -cl -cf    DOING
+1:  -ipc -cl -cf    NOT DONE
+2:  +ipc -cl -cf    NOT DONE
 3:  -ipc +cl -cf    NOT DONE
 4:  -ipc -cl +cf    NOT DONE
 5:  +ipc +cl -cf    NOT DONE
@@ -11,41 +11,25 @@
 
 import pdb
 from subprocess import call
+from subprocess import Popen, PIPE, STDOUT
 from os import listdir, getcwd
 from sys import argv
 from copy import deepcopy
+from astropy.io import fits
+import glob
 
 ncdhas      = '/usr/local/ncdhas/ncdhas'
 
-''' Setup Flag Structures '''
-''' Everett suggests ALWAYS having CBS on: (A + B2) - (A + B1) = B2 - B1 (+cbs removes A from all frames) '''
 
-''' start without CDS'''
-flags_all   = '+ow +wi +wd +ws -rx +rc -rss +rsf +cbp +cs +cbs -cd'
-
-p_ipc   = ' +ipc'
-m_ipc   = ' -ipc'
-p_cl    = ' +cl'
-m_cl    = ' -cl'
-p_cf    = ' +cf'
-m_cf    = ' -cf'
-
-flagsMMM    = flags_all + m_ipc + m_cl + m_cf  # 1  X
-flagsPMM    = flags_all + p_ipc + m_cl + m_cf  # 2  X
-flagsMPM    = flags_all + m_ipc + p_cl + m_cf  # 3  X
-flagsMMP    = flags_all + m_ipc + m_cl + p_cf  # 4  X
-flagsPPM    = flags_all + p_ipc + p_cl + m_cf  # 5  X
-flagsPMP    = flags_all + p_ipc + m_cl + p_cf  # 6  X
-flagsMPP    = flags_all + m_ipc + p_cl + p_cf  # 7  X
-flagsPPP    = flags_all + p_ipc + p_cl + p_cf  # 8  X
 
 ''' Setup File Structures '''
-basedir     = '/data/External/ISIMCV3_unzipped/NRCN821/fitsfilesonly/'
+basedir     = '/data1/tso_analysis/all_tso_cv3/'
 if basedir == getcwd():
     basedir = ''
 
-rawdir      = 'raw_separated_'
-rawdir      = rawdir + argv[1] + '/'
+#rawdir      = 'raw_separated_'
+#rawdir      = rawdir + argv[1] + '/'
+rawdir = 'one_file_tests/'
 
 set_raw_dirs= listdir(basedir + rawdir)
 
@@ -64,46 +48,46 @@ for dirNow in set_raw_dirs:
 reddir      = 'reduced_separated_'
 reddir      = reddir + argv[1] + '/'
 
-exec("flagsNOW    = flags" + argv[1])
+''' Setup Flag Structures '''
+''' Everett suggests ALWAYS having CBS on: (A + B2) - (A + B1) = B2 - B1 (+cbs removes A from all frames) '''
 
-# ''' Now run without CDS '''
-# for dirNow in raw_files.keys():
-#     for filename in raw_files[dirNow]:
-#         fileNOW     = basedir + rawdir + dirNow + '/' + filename
-#         call([ncdhas, fileNOW, flagsNOW])
-
-# for dirNow in raw_files.keys():
-#     red_files_list = basedir + rawdir + dirNow + '/' + '*.*.fits'
-#     call(['/bin/mkdir', '-p', basedir + reddir + dirNow])
-#     call(['/bin/mv'   , red_files_list, basedir + reddir + dirNow + '/' ])
-
-''' Now run with CDS '''
-flags_all   = '+ow -wi -wd +ws -rx +rc -rss +rsf +cbp +cs +cbs -cd +cds'
+''' Now run without CDS (we can always take plane [1] - plane[0] of the red image'''
+flags_all   = '+cfg isimcv3 +ow -wi -wd +ws -rx +rc -rss +rsf +cbp +cs +cbs -cd'
 
 p_ipc   = ' +ipc'
 m_ipc   = ' -ipc'
 p_cl    = ' +cl'
 m_cl    = ' -cl'
-p_cf    = ' +cf'
+p_cf    = ' +cf '
 m_cf    = ' -cf'
 
-flagsMMM    = flags_all + m_ipc + m_cl + m_cf  # 1  X
-flagsPMM    = flags_all + p_ipc + m_cl + m_cf  # 2  X
-flagsMPM    = flags_all + m_ipc + p_cl + m_cf  # 3  X
-flagsMMP    = flags_all + m_ipc + m_cl + p_cf  # 4  X
-flagsPPM    = flags_all + p_ipc + p_cl + m_cf  # 5  X
-flagsPMP    = flags_all + p_ipc + m_cl + p_cf  # 6  X
-flagsMPP    = flags_all + m_ipc + p_cl + p_cf  # 7  X
-flagsPPP    = flags_all + p_ipc + p_cl + p_cf  # 8  X
+'NRCA3_17012_PFlat_F150W_CLEAR_2016-04-05_imgonly.fits'
 
-exec("flagsNOW    = flags" + argv[1])
-# flagsNOW    = flagsNOW
+flagopt = {}
+flagopt['MMM']    = flags_all + m_ipc + m_cl + m_cf  # 1  X
+flagopt['PMM']    = flags_all + p_ipc + m_cl + m_cf  # 2  X
+flagopt['MPM']    = flags_all + m_ipc + p_cl + m_cf  # 3  X
+flagopt['MMP']    = flags_all + m_ipc + m_cl + p_cf  # 4  X
+flagopt['PPM']    = flags_all + p_ipc + p_cl + m_cf  # 5  X
+flagopt['PMP']    = flags_all + p_ipc + m_cl + p_cf  # 6  X
+flagopt['MPP']    = flags_all + m_ipc + p_cl + p_cf  # 7  X
+flagopt['PPP']    = flags_all + p_ipc + p_cl + p_cf  # 8  X
+
+flags_end   = ''
+
+flagsNOW    = flagopt[argv[1]] + flags_end
+
+flatdir = '/data1/tso_analysis/all_tso_cv3/flat_data/'
 
 for dirNow in raw_files.keys():
     for filename in raw_files[dirNow]:
         fileNOW     = basedir + rawdir + dirNow + '/' + filename
-        pdb.set_trace()
-        call([ncdhas, fileNOW, flagsNOW])
+        head = fits.getheader(fileNOW)
+        if argv[1][2] == 'P':
+            flatname = glob.glob(flatdir+head['DETECTOR']+'*.fits')
+            flagsNOW = flagsNOW +' +FFf '+ flatname[0]
+        #pdb.set_trace()
+        call(ncdhas+' '+fileNOW+' '+flagsNOW,shell=True)
 
 # for dirNow in raw_files.keys():
 #     red_files_list = basedir + rawdir + dirNow + '/' + '*.*.fits'
