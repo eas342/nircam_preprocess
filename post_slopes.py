fd89ccccbf0fb64684fea1e331714e879975c1f1
Everett Schlawin <eas342@email.arizona.edu>
2019-10-28 17:14:26 -0700

    Now each SCA can have customized col-by-col and row-by-row subtractions. Also, spelled out the col-by-col and row-by-row subtractions since they were confusing in the generalized loop

--- post_slopes.py
+++ post_slopes.py
@@ -28,9 +28,15 @@ for onePath in [outputDir,flatDir,dividedDir]:
 
 
 if 'rowsUse' not in pipeParam:
-    pipeParam['rowsUse'] = [[0,80],[140,256]]
+    pipeParam['rowsUse'] = {487:[[0,80],[140,256]],
+                            488:[[0,80],[140,256]],
+                            489:[[0,80],[140,256]],
+                            490:[[0,80],[140,256]]}
 if 'colsUse' not in pipeParam:
-    pipeParam['colsUse'] = [[4,190]]
+    pipeParam['colsUse'] = {487: [[4,190]],
+                            488: [[4,190]],
+                            489: [[4,190]],
+                            490: [[4,190]]}
 
 
 if 'indices_for_flatfield' not in pipeParam:
@@ -38,54 +44,119 @@ if 'indices_for_flatfield' not in pipeParam:
 
 
 def do_subtraction(inputPath,outFilePath,diagnostics=False):
+    """
+
+    Do a column-by-column, row-by-row or both subtractions
+    The column-by-column can remove amplifier offsets
+    The row-by-row can remove 1/f noise noise.
+
+    Indices are explained in this table:
+    
+    Type of subtraction | Direction of mask | Direction along
+                        |  coordinates      | which to median
+    ----------------------------------------------------------------
+    column-by-column    |  Y / 0  'rowsUse' |   Y / 0
+    row-by-row          |  X / 0  'colsUse' |   X / 1
+    """
     fileName = os.path.basename(inputPath)
     HDUList = fits.open(inputPath)
     dat = HDUList[0].data
     img = dat[0]
+    head = HDUList[0].header
     
+    ## Look up which rows and columns to median based on SCA ID
+    ## (different for different images)
+    if 'SCA_ID' not in head:
+        raise Exception("Could not find SCA ID")
+    elif head['SCA_ID'] not in pipeParam['rowsUse'].keys():
+        raise Exception("Could not find rows for SCA {}".format(head['SCA_ID']))
+    elif head['SCA_ID'] not in pipeParam['colsUse'].keys():
+        raise Exception("Could not find columns for SCA {}".format(head['SCA_ID']))
+    else:
+        rowsToMedian = pipeParam['rowsUse'][head['SCA_ID']]
+        colsToMedian = pipeParam['colsUse'][head['SCA_ID']]
     
-    rowsAndCols = [pipeParam['rowsUse'],pipeParam['colsUse']]
-    
-    x,y = np.meshgrid(np.arange(img.shape[1]),np.arange(img.shape[0]))
-    allCoords = [y,x]
-    medianDirections = [0,1] ## perpendicular to the coordinate dimensions
+    ## Copy the original image
     correctedImage = deepcopy(img)
-    for oneDim in [1,0]: ## Python does X=1, Y=0
-        # Start with column-by-column, so the coordinate direction is X
-        # to get rid of pre-amp resets
-        # This means that we use a mask for the X pixels
-        
-        indArray = allCoords[oneDim]
+    ## make a coordinate grid
+    x,y = np.meshgrid(np.arange(img.shape[1]),np.arange(img.shape[0]))
+    
+    
+    
+    if diagnostics == True:
+        primHDU = fits.PrimaryHDU(np.array(img))
+        primHDU.writeto('diagnostics/'+fileName+'_original.fits',overwrite=True)
+    
+    ## Do the column-by-column subtraction
+    if rowsToMedian is not None:
         pts = np.zeros_like(img,dtype=np.bool)
-        for oneRange in rowsAndCols[oneDim]:
-            thisRangePts = (indArray >= oneRange[0]) & (indArray <= oneRange[1])
+        ## gather coordinates (could be a disjointed list of them) 
+        for oneRange in rowsToMedian:
+            thisRangePts = (y >= oneRange[0]) & (y <= oneRange[1])
             pts = pts | thisRangePts
+       
         if diagnostics == True:
             primHDU = fits.PrimaryHDU(np.array(pts,dtype=np.uint16))
-            primHDU.writeto('diagnostics/'+fileName+'_mask_coord_dim_{}.fits'.format(oneDim),overwrite=True)
+            primHDU.writeto('diagnostics/'+fileName+'_mask_coord_col_by_col.fits',overwrite=True)
         
+        ## make a mask
         maskedImg = deepcopy(correctedImage) ## apply to the latest corrected image
         maskedImg[pts == False] = np.nan
-        medianThisDirection = np.nanmedian(maskedImg,axis=medianDirections[oneDim])
         
-        correction2D = np.tile(medianThisDirection,[img.shape[oneDim],1])
-        if medianDirections[oneDim] == 1:
-            ## for Y coordinates/ median along X, need to rotate this
-            correction2D = correction2D.transpose() 
+        ## find median and 2D correction
+        medianThisDirection = np.nanmedian(maskedImg,axis=0)
+        correction2D = np.tile(medianThisDirection,[img.shape[0],1])
         
         if diagnostics == True:
             primHDU = fits.PrimaryHDU(np.array(correction2D))
-            primHDU.writeto('diagnostics/'+fileName+'_correction_coord_dim_{}.fits'.format(oneDim),overwrite=True)
+            primHDU.writeto('diagnostics/'+fileName+'_correction_coord_dim_col_by_col.fits',overwrite=True)
         
         correctedImage = correctedImage - correction2D
-
+        
         if diagnostics == True:
             primHDU = fits.PrimaryHDU(np.array(correctedImage))
-            primHDU.writeto('diagnostics/'+fileName+'_corrrected_after_dim_{}.fits'.format(oneDim),overwrite=True)
+            primHDU.writeto('diagnostics/'+fileName+'_corrrected_after_dim_col_by_col.fits',overwrite=True)
+        
+        columnByColumn = True
+    else:
+        columnByColumn = True
+    
+    ## Do the row-by-row subtraction
+    if colsToMedian is not None:
+        pts = np.zeros_like(img,dtype=np.bool)
+        ## gather coordinates (could be a disjointed list of them)
+        for oneRange in colsToMedian:
+            thisRangePts = (x >= oneRange[0]) & (x <= oneRange[1])
+            pts = pts | thisRangePts
+        
+        if diagnostics == True:
+            primHDU = fits.PrimaryHDU(np.array(pts,dtype=np.uint16))
+            primHDU.writeto('diagnostics/'+fileName+'_mask_coord_row_by_row.fits',overwrite=True)
+        
+        maskedImg = deepcopy(correctedImage) ## apply to the latest corrected image
+        maskedImg[pts == False] = np.nan
+        medianThisDirection = np.nanmedian(maskedImg,axis=1)
+        
+        ## need to rotate this for the row-by-row median
+        correction2D = np.tile(medianThisDirection,[img.shape[1],1]).transpose()
+        
+        if diagnostics == True:
+            primHDU = fits.PrimaryHDU(np.array(correction2D))
+            primHDU.writeto('diagnostics/'+fileName+'_correction_coord_dim_row_by_row.fits',overwrite=True)
+        
+        correctedImage = correctedImage - correction2D
+        
+        if diagnostics == True:
+            primHDU = fits.PrimaryHDU(np.array(correctedImage))
+            primHDU.writeto('diagnostics/'+fileName+'_corrrected_after_dim_row_by_row.fits',overwrite=True)
+        
+        rowByRow = True
+    else:
+        rowByRow = False
     
     outHDU = fits.PrimaryHDU(correctedImage,HDUList[0].header)
-    outHDU.header['ROWBYROW'] = (True, 'Is a row-by-row median subtraction performed?')
-    outHDU.header['COLBYCOL'] = (True, 'Is a col-by-col median subtraction performed?')
+    outHDU.header['COLBYCOL'] = (columnByColumn, 'Is a col-by-col median subtraction performed?')
+    outHDU.header['ROWBYROW'] = (rowByRow, 'Is a row-by-row median subtraction performed?')
     
     outHDU.writeto(outFilePath,overwrite=True)
     HDUList.close()
@@ -93,11 +164,16 @@ def do_subtraction(inputPath,outFilePath,diagnostics=False):
     
 
 def subtract_all_files():
+    """
+    Do row and column subtraction on a set of files
+    
+    """
     testNames = os.listdir(procDir)
     for testInd,oneTest in enumerate(testNames):
         print("Working on Exposure {} ({} of {})".format(oneTest,testInd,len(testNames)))
         inputTestDir = os.path.join(procDir,oneTest)
         outputTestDir = os.path.join(outputDir,oneTest)
+        
         if os.path.exists(outputTestDir) == False:
             os.mkdir(outputTestDir)
         fileL = np.sort(glob.glob(os.path.join(inputTestDir,'*.slp.fits')))
